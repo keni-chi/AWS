9f9c8bcb6d1944bd5f8801153a7a317adaed0cfe
Kenichi.Nagahara <kenichi.nagahara@daikin.co.jp>
2019-03-03 23:12:28 +0900

    commit

---
+++ SQS/DLQ/input.py
@@ -0,0 +1,25 @@
+﻿import boto3
+from datetime import datetime
+
+SQS_NAME = 'nagatest'
+
+def lambda_handler(event, context):
+    sqs_sends()
+
+
+def sqs_sends():
+    print('Input--------------------start')
+    now = datetime.utcnow()
+    now_str = datetime.strftime(now, '%Y-%m-%dT%H:%M:%S.%fZ')
+    print('Input-start:  ' + now_str)
+
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName=SQS_NAME)
+    data = '{\"k\": \"v\"}'
+    msg_list = [{'Id': '1', 'MessageBody': data}]
+    response = queue.send_messages(Entries=msg_list)
+
+    now = datetime.utcnow()
+    now_str = datetime.strftime(now, '%Y-%m-%dT%H:%M:%S.%fZ')
+    print('Input-end:    ' + now_str)
+    print('Input--------------------end')
---
+++ SQS/DLQ/output.py
@@ -0,0 +1,14 @@
+﻿import json
+from datetime import datetime
+import time
+
+def lambda_handler(event, context):
+    
+    print('output--------------------start')
+    now = datetime.utcnow()
+    now_str = datetime.strftime(now, '%Y-%m-%dT%H:%M:%S.%fZ')
+    print('Output-start: ' + now_str)
+    x = {"a": 1}
+    y = x['b']  #エラー発生
+
+    print('output---------------end')
